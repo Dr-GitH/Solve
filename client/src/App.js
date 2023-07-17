66496a4de7e3d253df21bf884d29bc1b44176427
Dr-GitH <119149926+Dr-GitH@users.noreply.github.com>
2023-07-17 17:13:16 +0530

    Activity points updation(3)

--- client/src/App.js
+++ client/src/App.js
@@ -723,8 +723,7 @@ function SignUpForm({ handleSignUp }) {
 
   return (
     <div className="card">
-      {/* <div className="LoginHead">
-      <h1>Sign Up</h1></div> */}
+  
       <h2 className="card-heading">SIGN UP</h2>
       <form className="LoginPage" onSubmit={handleSubmit}>
         <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
